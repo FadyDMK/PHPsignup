fc37a5be8a321395cb265eba2a303a53604e4a66
Fady DMK <32818953+FadyDMK@users.noreply.github.com>
2023-06-03 13:21:09 +0200

    validation

--- process-signup.php
+++ process-signup.php
@@ -1,4 +1,33 @@
 <?php
 
-print_r($_POST["name"]);
+if (empty($_POST["name"])){
+    die("Name required");
+}
+
+if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+    die("Valid Email required");
+}
+
+if (strlen($_POST["password"])<8){
+    die("password must be atleast 8 characters long");
+}
+
+if (! preg_match("/[a-z]/i", $_POST["password"])){
+    die("password must contain atleast one letter");
+}
+
+if(!preg_match("/[1-9]/", $_POST["password"])){
+    die("password must contain atleast one digit");
+}
+
+if ($_POST["password"]!== $_POST["password_confirmation"]){
+    die("Passwords do not match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+
+
+print_r($_POST);
+var_dump($password_hash);
 ?>
